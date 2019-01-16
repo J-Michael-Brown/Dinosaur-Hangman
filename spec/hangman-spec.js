7db221b3613752a684ba1e023a4ef2823ea0b1ea
Daniel Burbach and Michael Brown <michael@>
2019-01-16 11:35:32 -0800

    add basic specs for hangman game

--- spec/hangman-spec.js
+++ spec/hangman-spec.js
@@ -1,14 +1,23 @@
 import { Hangman } from './../src/hangman.js';
 
 describe('Hangman', function() {
-  let newGame;
+  let dinosaur;
   beforeEach(function(){
-    newGame = new hangman();
+    dinosaur = new hangman('dinosaur');
   });
 
-  describe('', function() {
-    it('should', function() {
-      
+  describe('buildHidden', function() {
+    it('should split a word into an array of dashes.', function() {
+      expect(dinosaur.buildHidden()).toEqual(["-","-","-","-","-","-","-","-"]);
+    });
+  });
+
+  describe('guess', function() {
+    it('should return a true statement if the letter guessed in the array is true.', function() {
+      expect(dinosaur.guess("q")).toEqual(false);
+      expect(dinosaur.hiddenWordArray).toEqual(["-","-","-","-","-","-","-","-"]);
+      expect(dinosaur.guess("d")).toEqual(true);
+      expect(dinosaur.hiddenWordArray).toEqual(["d","-","-","-","-","-","-","-"]);
     });
   });
 
