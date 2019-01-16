a4fb928a53e6c9c752d1eefbdb2d556db3804e88
Daniel Burbach and Michael Brown <daniel@>
2019-01-16 13:55:35 -0800

    add body onLoad function call.

--- src/main.js
+++ src/main.js
@@ -4,38 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Hangman } from './hangman.js';
 
 $(document).ready(function() {
-  $.ajax({
-    url: 'http://dinoipsum.herokuapp.com/api/',
-    type: 'GET',
-    data: {
-      format: 'json',
-      paragraphs: '1',
-      words: '1'
-    },
-    success: function(response) {
-      console.log(response[0][0]);
-    },
-    error: function() {
-      console.log('api call failed.');
-    }
+  let word;
+  $("body").onLoad(function() {
+    $.ajax({
+      url: 'http://dinoipsum.herokuapp.com/api/',
+      type: 'GET',
+      data: {
+        format: 'json',
+        paragraphs: '1',
+        words: '1'
+      },
+      success: function(response) {
+        word = response[0][0];
+        runGame(word);
+      },
+      error: function() {
+        $('#error-message').text('There was a problem proccessing your API.');
+      }
+    });
   });
-  // let hangman = new Hangman();
-  // if (hangman.word){
-  // } else {
-  //   // $('#error-message').text('');
-  // }
-  //   if ((event.target.readyState === 'interactive') || (event.target.readyState === 'ready')) {
-  //     loadGame();
-  //     setInterval(() => {
-  //       hangman.timer--;
-  //       document.getElementById('timer').innerHTML = hangman.timer;}, 1000);
-  //   }
-  //
-  //   function loadGame() {
-  //     buildUI();
-  //   }
-  // });
-  //
-  // var xhr = $.get("http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1");
-  // xhr.done(function(data) { console.log("success got data", data);
+
+  function runGame(word) {
+    let hangman = new Hangman(word);
+    debugger;
+  }
 });
