5c523f7da9cc18daac2ac416c38b2010e1630a7e
Daniel Burbach and Michael Brown <daniel@>
2019-01-16 10:36:54 -0800

    api call returns an object.

--- src/hangman.js
+++ src/hangman.js
@@ -1,6 +1,23 @@
+import $ from 'jquery';
+
+
 class Hangman {
 
   constructor() {
+    let word;
+    $.ajax({
+      url: 'http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1',
+      type: 'GET',
+      data: {
+        format: 'json'
+      },
+      success: function(response) {
+        word = response[0][0];
+      },
+      error: function() {
+        word = false;
+      }
+    });
 
   }
 
--- src/main.js
+++ src/main.js
@@ -3,20 +3,39 @@ import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Hangman } from './hangman.js';
 
-let hangman = new Hangman();
-
-document.addEventListener('readystatechange', event => {
-  if ((event.target.readyState === 'interactive') || (event.target.readyState === 'ready')) {
-    loadGame();
-    setInterval(() => {
-      hangman.timer--;
-      document.getElementById('timer').innerHTML = hangman.timer;}, 1000);
-  }
-
-  function loadGame() {
-    buildUI();
-  }
+$(document).ready(function() {
+  $.ajax({
+    url: 'http://dinoipsum.herokuapp.com/api/',
+    type: 'GET',
+    data: {
+      format: 'json',
+      paragraphs: '1',
+      words: '1'
+    },
+    success: function(response) {
+      console.log(response[0][0]);
+    },
+    error: function() {
+      console.log('api call failed.');
+    }
+  });
+  // let hangman = new Hangman();
+  // if (hangman.word){
+  // } else {
+  //   // $('#error-message').text('');
+  // }
+  //   if ((event.target.readyState === 'interactive') || (event.target.readyState === 'ready')) {
+  //     loadGame();
+  //     setInterval(() => {
+  //       hangman.timer--;
+  //       document.getElementById('timer').innerHTML = hangman.timer;}, 1000);
+  //   }
+  //
+  //   function loadGame() {
+  //     buildUI();
+  //   }
+  // });
+  //
+  // var xhr = $.get("http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=1&words=1");
+  // xhr.done(function(data) { console.log("success got data", data);
 });
-
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
